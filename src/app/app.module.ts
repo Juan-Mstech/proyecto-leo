ab4265a007ad9292533f70a35083f8fc22762ec2
Juan <jponce@mstech.la>
2019-06-19 15:05:28 -0300

    Mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TablaComponent } from './components/tabla/tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    SettingsComponent,
+    FooterComponent,
+    TablaComponent,
   ],
   imports: [
     BrowserModule,
