40c445b7aa10e91baa7acafd7a21c2902aa7ed50
Juan-Mstech <jponce@mstech.la>
2019-06-22 11:21:00 -0300

    22.06.19-11.20

--- src/app/components/presentacion-pctos/presentacion-pctos.component.ts
+++ src/app/components/presentacion-pctos/presentacion-pctos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductosServices} from '../../productos.services';
-import {Products} from '../../model/products';
+import { ProductosServices } from '../../productos.services';
+import { Products } from '../../model/products';
 
 @Component({
   selector: 'app-presentacion-pctos',
@@ -10,9 +10,9 @@ import {Products} from '../../model/products';
 })
 export class PresentacionPctosComponent implements OnInit {
   listaProducto: Array<Products>;
-  constructor(servicio:ProductosServices) {
-    this.listaProducto=servicio.getProductos();
-   }
+  constructor(servicio: ProductosServices) {
+    this.listaProducto = servicio.getProductos();
+  }
 
   ngOnInit() {
   }
