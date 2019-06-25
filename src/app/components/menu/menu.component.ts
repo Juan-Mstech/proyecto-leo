7d2009ba574211e23f6383beb22ae1f24b662161
Juan <jponce@mstech.la>
2019-06-25 11:42:22 -0300

    25.06.19-11.42

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,20 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductosServices } from '../../productos.services';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css'],
-  providers: [ProductosServices]
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
-  listaTipoProductos: string[];
-  listaMarcas: string[];
-  constructor(servicio: ProductosServices) {
-    this.listaTipoProductos = servicio.getTipoProductos();
-    this.listaMarcas = servicio.getMarcas();
-  }
+  constructor() { }
 
   ngOnInit() {
   }
